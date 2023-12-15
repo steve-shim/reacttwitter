c89ddf12032c3abd5e25311b4961eb44f6d5ce7c
steve-shim <steveshim496@gmail.com>
2023-12-15 21:49:44 +0900

    Add User auth in context

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Router from "components/Router";
 import { Layout } from "components/Layout";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/loader/Loader";
 
 function App() {
   const auth = getAuth(app);
-  console.log("[App] auth", auth, !!auth?.currentUser);
+  console.log("[App] auth", !!auth?.currentUser, auth);
 
+  const [init, setInit] = useState<boolean>(false);
   // auth의 currentUser가 있으면 authenticated로 변경
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    console.log("[App] useEffect", !!auth?.currentUser, auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
-      <Router isAuthenticated={isAuthenticated} />
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
